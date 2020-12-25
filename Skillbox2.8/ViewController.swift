e677cbceb0f0dc9b65be7949e0607fe7371e4e9d
Artem Akopian <artem7498@gmail.com>
2020-12-25 14:49:41 +1000

    Initial Commit

---
+++ Skillbox2.8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skillbox2.8
+//
+//  Created by Артём on 12/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
